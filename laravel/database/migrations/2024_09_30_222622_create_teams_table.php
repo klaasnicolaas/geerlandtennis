f3eb1192963378b09a7825159e610bceae815d42
Klaas Schoute <hello@student-techlife.com>
2024-10-14 03:32:47 +0200

    Refactor the TeamResource table and form (#24)

--- laravel/database/migrations/2024_09_30_222622_create_teams_table.php
+++ laravel/database/migrations/2024_09_30_222622_create_teams_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         // Create the teams table.
         Schema::create('teams', function (Blueprint $table): void {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
 
