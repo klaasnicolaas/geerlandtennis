40108f24b73ecfc32bdaac9aab43f67ebb21f14e
Klaas Schoute <hello@student-techlife.com>
2024-10-15 22:27:03 +0200

    Add winner_team_id to Sets resource in Filament

--- laravel/database/migrations/2024_10_03_233700_create_tennis_sets_table.php
+++ laravel/database/migrations/2024_10_03_233700_create_tennis_sets_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tennis_sets', function (Blueprint $table): void {
             $table->id();
             $table->foreignId('tennis_match_id')->constrained('tennis_matches')->onDelete('cascade');
+            $table->foreignId('winner_team_id')->nullable()->constrained('teams')->onDelete('set null');
             $table->integer('set_number');
 
             // Scores for each team in this set
