c6ab2129fb8661d3ce8f16e1e9f636883173d30a
Klaas Schoute <hello@student-techlife.com>
2024-10-16 00:14:13 +0200

    Add view page for tennis matches (#35)
    * Add view page for tennis matches
    
    * Run Laravel pint

---
+++ laravel/app/Filament/Resources/TennisMatchResource/Pages/ViewTennisMatch.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\TennisMatchResource\Pages;
+
+use App\Filament\Resources\TennisMatchResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewTennisMatch extends ViewRecord
+{
+    protected static string $resource = TennisMatchResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('back')
+                ->url(TennisMatchResource::getUrl())
+                ->button()
+                ->icon('heroicon-o-chevron-left')
+                ->color('info'),
+            Actions\EditAction::make(),
+        ];
+    }
+}
