714100268b511a702c430fea253484dbb62fc861
Klaas Schoute <hello@student-techlife.com>
2024-10-20 23:33:34 +0200

    Refactor and add Laravel policies (#44)
    * Refactor the way of creating permissions for Filament Shield seeder
    
    * Add Laravel policies for some models
    
    * Run Laravel pint

---
+++ laravel/app/Policies/TennisSetPolicy.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\TennisSet;
+use App\Models\User;
+
+class TennisSetPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_tennis::set');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, TennisSet $tennisSet): bool
+    {
+        return $user->can('view_tennis::set');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_tennis::set');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, TennisSet $tennisSet): bool
+    {
+        return $user->can('update_tennis::set');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, TennisSet $tennisSet): bool
+    {
+        return $user->can('delete_tennis::set');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    // public function restore(User $user, TennisSet $tennisSet): bool
+    // {
+    //     return $user->can('restore_tennis::set');
+    // }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    // public function forceDelete(User $user, TennisSet $tennisSet): bool
+    // {
+    //     return $user->can('force_delete_tennis::set');
+    // }
+}
---
+++ laravel/config/shield-permissions.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'roles' => [
+        'admin' => [
+            'guard_name' => 'web',
+            'permissions' => [
+                'resource' => [
+                    'role' => ['view', 'view_any', 'create', 'update', 'delete', 'delete_any'],
+                    'user' => ['view', 'view_any', 'create', 'update', 'delete', 'delete_any', 'restore', 'restore_any', 'replicate', 'reorder', 'force_delete', 'force_delete_any'],
+                    'team' => ['view', 'view_any', 'create', 'update', 'delete', 'delete_any'],
+                    'tennis-match' => ['view', 'view_any', 'create', 'update', 'delete', 'delete_any'],
+                    'tennis-set' => ['view', 'view_any', 'create', 'update', 'delete', 'delete_any'],
+                    'tournament' => ['view', 'view_any', 'create', 'update', 'delete', 'delete_any'],
+                ],
+                'special' => [
+                    'download-backup',
+                    'delete-backup',
+                ],
+                'page' => [
+                    'Backups',
+                ],
+            ],
+        ],
+        'moderator' => [
+            'guard_name' => 'web',
+            'permissions' => [
+                'resource' => [
+                    'role' => ['view', 'view_any'],
+                    'user' => ['view', 'view_any'],
+                ],
+            ],
+        ],
+        'user' => [
+            'guard_name' => 'web',
+            'permissions' => [],
+        ],
+    ],
+    'direct_permissions' => [
+        'download-backup' => 'web',
+        'delete-backup' => 'web',
+    ],
+];
